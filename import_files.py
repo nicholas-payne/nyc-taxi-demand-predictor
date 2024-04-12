7fa956bdaf04ea58085f43ecb3639d3815924a5c
nicholas-payne <60719671+nicholas-payne@users.noreply.github.com>
2024-04-12 16:11:20 -0400

    Create import_files.py
    first draft of import data script

---
+++ import_files.py
@@ -0,0 +1,72 @@
+import wget
+import os
+import pyspark
+from pyspark.sql import SQLContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import date_trunc,expr,unix_timestamp,extract,lit
+import itertools as it
+
+def scrape_data():
+
+    ''' 
+    Downloads the raw taxi ride data from the NYC Taxi and Limousine Commission as parquet files 
+    The data is stored in parquet format to reduce filesize. The full dataset is close to 300GB in size
+    so the script may take a while to run
+    '''
+    # Full Range of Data Here #
+    #years = range(2009,2024)
+    #months = range(1,13)
+    ###########################
+
+    # Limited Sample of Data for Testing #
+    years = [2013]
+    months = [1,2]
+    ######################################
+
+    ym_list = list(it.product(years,months))
+
+    for ym in ym_list:
+         url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{ym[0]}-{ym[1]:02d}.parquet"
+         filepath = f"yellow_tripdata_{ym[0]}-{ym[1]:02d}.parquet"
+         wget.download(url, filepath)
+                  
+         spark = SparkSession.builder \
+            .master('local') \
+            .appName('myAppName') \
+            .config('spark.executor.memory', '5gb') \
+            .config("spark.cores.max", "6") \
+            .getOrCreate()
+         sc = spark.sparkContext
+
+         sqlContext = SQLContext(sc)
+
+         df = sqlContext.read.parquet(f'yellow_tripdata_{ym[0]}-{ym[1]:02d}.parquet')
+
+         df_filtered = df \
+            .select(
+                date_trunc('hour', df.tpep_pickup_datetime).alias('pickup_hour'),
+                "PULocationID",
+                "trip_distance",
+                "fare_amount",
+                "payment_type"
+                ) \
+            .filter(
+                (extract(lit('YEAR'),df.tpep_pickup_datetime) == ym[0]) &
+                ((unix_timestamp(df.tpep_dropoff_datetime) - unix_timestamp(df.tpep_pickup_datetime))/60 > 0) &
+                (df.RatecodeID != '5.0') &
+                (df.trip_distance > 0) &
+                (df.payment_type.isin(['1','2']))
+                )
+
+         df_agg = df_filtered \
+             .groupBy("pickup_hour","PULocationID") \
+             .agg({"trip_distance": "avg","fare_amount":"avg","payment_type":"count"}) \
+             .orderBy(["pickup_hour","PULocationID"])
+
+         df_agg_pandas = df_agg.toPandas()
+         df_agg_pandas.to_csv(f'agg_taxi_data_{ym[0]}_{ym[1]:02d}.csv')
+
+         os.remove(f'yellow_tripdata_{ym[0]}-{ym[1]:02d}.parquet')
+
+if __name__ == '__main__':
+    scrape_data()
